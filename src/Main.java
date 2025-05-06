b1db6e2a306645612a44d40ffb85af275b9bd2e5
hopegaju <hopegajupr@gmail.com>
2025-05-06 10:15:56 +0200

    .equals()

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Pair<String, Integer> student = new Pair<>("Age", 10);
+        Pair<Integer,String> error= new Pair<>(404, "Not found");
+        System.out.println(student);
+        System.out.println(error);
+        System.out.println(student.equals(error));
     }
 }
