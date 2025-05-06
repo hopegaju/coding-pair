8891a9e6f396234715c429765d60a6909172f0aa
hopegaju <hopegajupr@gmail.com>
2025-05-06 10:26:06 +0200

    Use of hashSet

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.HashSet;
+
 public class Main {
     public static void main(String[] args) {
         Pair<String, Integer> student = new Pair<>("Age", 10);
@@ -5,5 +7,8 @@ public class Main {
         System.out.println(student);
         System.out.println(error);
         System.out.println(student.equals(error));
+        HashSet<Pair<String,Integer>> hashSet = new HashSet<>();
+        hashSet.add(student);
+        System.out.println(hashSet);
     }
 }
